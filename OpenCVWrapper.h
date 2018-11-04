edc418782e916702143714676e2a27b699633e0c
Elijah Peake <37449363+epeake@users.noreply.github.com>
2018-11-04 16:22:49 -0500

    Add files via upload

---
+++ OpenCVWrapper.h
@@ -0,0 +1,13 @@
+//
+//  OpenCVWrapper.h
+//  quiccPrints
+//
+//  Created by Elijah Peake on 10/27/18.
+//  Copyright © 2018 Elijah Peake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OpenCVWrapper : UIViewController
+
+@end
